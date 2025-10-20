a99e97cf99d95efbe62e3d165852e23eca85e8f5
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-10-20 11:18:41 +0600

    packages update

--- index.js
+++ index.js
@@ -4,36 +4,39 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const authRoutes = require("./src/routes/auth.routes.js")
-const taskRoutes = require("./src/routes/task.routes.js")
-
-
+const authRoutes = require("./src/routes/auth.routes.js");
+const taskRoutes = require("./src/routes/task.routes.js");
 
 const app = express();
 const corsConfig = {
-    origin: true,
-    credentials: true,
+  origin: true,
+  credentials: true,
 };
 app.use(cors(corsConfig));
 
 app.use(express.json());
-app.options("*", cors(corsConfig));
+app.options(/.*/, cors(corsConfig));
 
+app.use("/auth", authRoutes);
+app.use("/task", taskRoutes);
 
-app.use("/auth", authRoutes)
-app.use("/task", taskRoutes)
+app.use("/", (req, res) => {
+  res.send("Hello world");
+});
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 // connect with mongodb
 
 const PORT = process.env.PORT || 8080;
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => {
-        app.listen(PORT, () => console.log(`server has been connected to ${PORT}`))
-    })
-    .catch((error) => console.log(error, `did not connect to ${PORT}`))
-
-
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server has been connected to ${PORT}`));
+  })
+  .catch((error) => console.log(error, `did not connect to ${PORT}`));
