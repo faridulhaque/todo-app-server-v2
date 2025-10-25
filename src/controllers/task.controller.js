073edf9da2e873cac0d2e82872fc4d534c6e279d
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-10-25 15:49:52 +0600

    query updated

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,80 +1,91 @@
 const TaskModel = require("../models/task.model");
-const moment = require('moment');
-
-
+const moment = require("moment");
 
 const addTask = async (req, res, next) => {
-    try {
-        const newTask = new TaskModel(req.body)
-        const data = await newTask.save();
-        res.status(200).json(data);
+  try {
+    const newTask = new TaskModel(req.body);
+    const data = await newTask.save();
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    } catch (error) {
-        next(error);
-    }
-}
+const getUpcomingTasks = async (req, res, next) => {
+  try {
+    const email = req.params.email;
+    const today = moment().format("YYYY-MM-DD");
 
-
-const getAllTask = async (req, res, next) => {
-    try {
-        const email = req.params.email;
-        const tasks = await TaskModel.find({ email })
-            .sort({ date: 1, time: 1 })
-            .exec();
-        res.status(200).json(tasks);
-    } catch (error) {
-        next(error);
-    }
-}
-
+    const tasks = await TaskModel.find({ email, date: { $gt: today } })
+      .sort({ date: 1, time: 1 })
+      .exec();
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getTodayTasks = async (req, res, next) => {
-    try {
-        const email = req.params.email;
+  try {
+    const email = req.params.email;
 
-        const today = moment().format('YYYY-MM-DD');
-        const tasks = await TaskModel.find({ email, complete: false});
-        res.status(200).json(tasks);
-    } catch (error) {
-        next(error);
-    }
+    const today = moment().format("YYYY-MM-DD");
+    const tasks = await TaskModel.find({ email, date: today }).sort({
+      time: -1,
+    });
+    res.status(200).json(tasks);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getArchiveTasks = async (req, res, next) => {
-    try {
-        const email = req.params.email;
-        if (email) {
-            const tasks = await TaskModel.find({ email, complete: true }).sort({ date: -1, time: -1 });
-            res.status(200).json(tasks);
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const email = req.params.email;
+    if (email) {
+      const today = moment().format("YYYY-MM-DD");
+
+      const tasks = await TaskModel.find({ email, date: { $lt: today } }).sort({
+        date: -1,
+        time: -1,
+      });
+      res.status(200).json(tasks);
     }
+  } catch (error) {
+    next(error);
+  }
 };
 
 const changeStatus = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const task = await TaskModel.findByIdAndUpdate(id, { complete: req.body.complete });
-        res.status(200).json(task);
-    } catch (error) {
-        next(error);
-    }
-};
+  try {
+    const id = req.params.id;
+    console.log("id", id);
+    const task = await TaskModel.findById(id);
 
+    const uTask = await TaskModel.findByIdAndUpdate(id, {
+      complete: !task.complete,
+    });
+    res.status(200).json(uTask);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteTask = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const result = await TaskModel.findByIdAndDelete(id);
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const id = req.params.id;
+    const result = await TaskModel.findByIdAndDelete(id);
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
-
-
-
-
-module.exports = { addTask, getAllTask, getTodayTasks, getArchiveTasks, changeStatus, deleteTask }
+module.exports = {
+  addTask,
+  getUpcomingTasks,
+  getTodayTasks,
+  getArchiveTasks,
+  changeStatus,
+  deleteTask,
+};
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,15 +1,21 @@
+const express = require("express");
+const {
+  addTask,
+  getTodayTasks,
+  getArchiveTasks,
+  changeStatus,
+  deleteTask,
+  getUpcomingTasks,
+} = require("../controllers/task.controller");
+const { commonError } = require("../middleWares/commonError");
 
-const express = require('express');
-const { addTask, getAllTask, getTodayTasks, getArchiveTasks, changeStatus, deleteTask } = require('../controllers/task.controller');
-const { commonError } = require('../middleWares/commonError');
+const router = express.Router();
 
-const router = express.Router()
-
-router.post('/add', addTask, commonError)
-router.get('/all/:email', getAllTask, commonError)
-router.get('/today/:email', getTodayTasks, commonError)
-router.get('/archive/:email', getArchiveTasks, commonError)
-router.put('/status/:id', changeStatus, commonError)
-router.delete('/del/:id', deleteTask, commonError)
+router.post("/add", addTask, commonError);
+router.get("/upcoming/:email", getUpcomingTasks, commonError);
+router.get("/today/:email", getTodayTasks, commonError);
+router.get("/previous/:email", getArchiveTasks, commonError);
+router.put("/status/:id", changeStatus, commonError);
+router.delete("/del/:id", deleteTask, commonError);
 
 module.exports = router;
